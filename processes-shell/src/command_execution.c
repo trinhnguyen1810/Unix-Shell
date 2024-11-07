4f7e4bc1acd5fa39ca54b9efd136f0b52d8eb79b
trinhnguyen1810 <trinhnguyen@uni.minerva.edu>
2024-11-07 05:44:45 -0800

    update

--- processes-shell/src/command_execution.c
+++ processes-shell/src/command_execution.c
@@ -3,19 +3,18 @@
 #include "system_commands.h"
 #include "history.h"
 
-
 /* Executes a command based on user input by checking for built-in commands 
 or handling external commands if needed. */
 void commandExecution(char *args[], int numberArgs, FILE *out)
 {
-    // check for empty command
+    // Check for empty command
     if (args == NULL || args[0] == NULL)
     {
         displayError();
         return;
     }
 
-    // process built-in commands
+    // Process built-in commands
     if (strcmp(args[0], "exit") == 0)
     {
         processExit(numberArgs);
@@ -38,7 +37,7 @@ void commandExecution(char *args[], int numberArgs, FILE *out)
     }
     else if (args[0][0] == '!')
     {
-        // handle history recall command
+        // Handle history recall command
         if (numberArgs == 1)
         {
             int historyIndex = atoi(args[0] + 1) - 1;
@@ -58,60 +57,67 @@ void commandExecution(char *args[], int numberArgs, FILE *out)
     }
     else
     {
-        // handle external command
+        // Handle external command
         runExternalCommand(args, out);
     }
 }
 
 /* Function to parse and execute a command by tokenizing the input
- and passing the arguments to the command execution function.*/
-void runCommand(char *cmds) {
-    char *argsList[256];  
-    int argCount = 0;     
-    char *token;          
+ and passing the arguments to the command execution function. */
+void runCommand(char *cmds)
+{
+    char *argsList[256];  // Arguments list
+    int argCount = 0;     // Number of arguments
+    char *token;          // Temporary token
 
-    //tokenize the input and store tokens in argsList
-    while ((token = strsep(&cmds, " ")) != NULL) {
-        if (token[0] != '\0') {
+    // Tokenize the input and store tokens in argsList
+    while ((token = strsep(&cmds, " ")) != NULL)
+    {
+        if (token[0] != '\0')
+        {
             argsList[argCount++] = token;
         }
     }
-    argsList[argCount] = NULL;  
+    argsList[argCount] = NULL;  // Null terminate the arguments array
 
-    // execute the command with the parsed arguments
+    // Execute the command with the parsed arguments
     commandExecution(argsList, argCount, stdout);
 }
 
-/* Parses and executes a command in a new thread, handling redirection if needed.*/
-void *parseCommandThread(void *arg) {
-    //retrieve the command and initialize output as stdout
+/* Parses and executes a command in a new thread, handling redirection if needed. */
+void *parseCommandThread(void *arg)
+{
+    // Retrieve the command and initialize output as stdout
     struct CommandArgs *arguments = (struct CommandArgs *)arg;
     char *cmdLine = arguments->cmds;
     FILE *output = stdout;
     int numArgs = 0;
 
-    //handle potential redirection in the command line
+    // Handle potential redirection in the command line
     cmdLine = parseCmdWithRedirection(&cmdLine, &output);
-    if (cmdLine == NULL) {
+    if (cmdLine == NULL)
+    {
         return NULL;
     }
 
-    //split the command line into arguments
+    // Split the command line into arguments
     char **args = splitArgs(cmdLine, &numArgs);
-    if (numArgs > 0) {
-        //execute the command if there are arguments
+    if (numArgs > 0)
+    {
+        // Execute the command if there are arguments
         commandExecution(args, numArgs, output);
     }
 
-    //close the output file if redirection was applied
-    if (output != stdout) {
+    // Close the output file if redirection was applied
+    if (output != stdout)
+    {
         fclose(output);
     }
 
     return NULL;
 }
 
-/* Runs an external command by forking a new process */
+/* Runs an external command by forking a new process. */
 void runExternalCommand(char *args[], FILE *out)
 {
     char command_path[256];
@@ -124,25 +130,25 @@ void runExternalCommand(char *args[], FILE *out)
     pid_t pid = fork();
     if (pid == -1)
     {
-        displayError(); // fork failed
+        displayError(); // Fork failed
     }
     else if (pid == 0)
     {
-        manageChildProcess(command_path, args, out); // child process execution
+        manageChildProcess(command_path, args, out); // Child process execution
     }
     else
     {
-        waitpid(pid, NULL, 0); // parent waits for the child
+        waitpid(pid, NULL, 0); // Parent waits for the child
     }
 }
 
-/* Manages child process execution by setting up redirection and executing the command */
+/* Manages child process execution by setting up redirection and executing the command. */
 static void manageChildProcess(char *command_path, char *args[], FILE *out)
 {
-   redirection(out); // redirects output if needed
-   if (execv(command_path, args) == -1)
-   {
-       displayError(); // if execv fails, display error and exit
-       exit(EXIT_FAILURE);
-   }
+    redirection(out); // Redirects output if needed
+    if (execv(command_path, args) == -1)
+    {
+        displayError(); // If execv fails, display error and exit
+        exit(EXIT_FAILURE);
+    }
 }
